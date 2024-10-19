e2f8f7cd9d1d32816de3139282aa1ac848944fba
Laevskiy <Laevskiy@mail.ru>
2024-10-19 11:39:34 +0300

    proba2

---
+++ src/HM/4/Input.tsx
@@ -0,0 +1,15 @@
+type InputPropsType = {
+    currentText: string // НУЖНО ПРОТИПИЗИРОВАТЬ
+    setCurrentText: (a:string)=>void// НУЖНО ПРОТИПИЗИРОВАТЬ
+};
+
+export const Input = (props: InputPropsType) => {
+    const onChangeHandler = (event: string) => {
+        // НУЖНО ДОПИСАТЬ
+        props.setCurrentText(event)
+    };
+
+     return (
+       <input id={'hw04-input'} type="text" value={props.currentText} onChange={(e)=>onChangeHandler(e.target.value)} />
+     );
+};
